a185229aa598fa86035115015042622a80cacb7c
EternalEnvy <lee.southerst@gmail.com>
2015-09-28 13:22:12 -0400

    asdf

---
+++ SmackBrosMatchmakingServer/SmackBrosMatchmakingServer/QueueAcceptedJoinPacket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SmackBrosMatchmakingServer
+{
+    class QueueStatusUpdatePacket : Packet
+    {
+        public bool Accepted;
+        public override void WritePacketData(List<byte> stream)
+        {
+
+        }
+        public override void ReadPacketData(Stream stream)
+        {
+        }
+    }
+}
